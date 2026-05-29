98f9b67c05c507e0fb0a911915cc6f59d125d71c
Matheus Nunes de Souza <matheus.n.souza10@aluno.senai.br>
2026-05-29 13:45:34 -0300

    Add files via upload

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,6 @@
 let footerBtn = document.querySelector('.footer-bottom .btn-icon');
 
-footerBtn.addEventListener('click', () => {
+footerBtn?.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
         left: 0,
